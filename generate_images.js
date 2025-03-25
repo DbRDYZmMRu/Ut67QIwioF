e5b025c330ba766cc3a0663af2240bf4c7c27e73
DbRDYZmMRu <frithmangrove@gmail.com>
2025-03-25 17:25:35 +0100

    Update generate_images.js

--- generate_images.js
+++ generate_images.js
@@ -7,24 +7,46 @@ registerFont('font/Architects_Daughter.ttf', { family: 'Architects Daughter' });
 
 const imageSize = 1080;
 const bgImagePath = 'https://raw.githubusercontent.com/DbRDYZmMRu/Ut67QIwioF/refs/heads/main/images/share/bg.jpg';
-const outputDir = path.join(__dirname, 'images', 'share', 'FHC', '2');
+const outputDir = path.join(__dirname, 'images', 'share', 'FHC', '3');
 const titles = [
-              "Radio",
-              "Generators have no Degree",
-              "Hirsute Rams",
-              "I Drew",
-              "Stories",
-              "Alison’s Band",
-              "Blank Space",
-              "Virgae",
-              "Welkin",
-              "Potch Shoe",
-              "Enwreathe",
-              "Stalwart",
-              "Defilade",
-              "On a Hot Day",
-              "On the Dromos for the Grith",
-              "West to West"
+              "Mirrors",
+"Steady",
+"Offering off a Lark",
+"The Masked Girl",
+"Dough",
+"The Brazen Spider, Unwanted Nut",
+"The Bug, the First Car",
+"Driver Licence",
+"A Pitch Run",
+"Caspian’s Autumn Foam",
+"Before it’s Time",
+"Upside Down Shower",
+"Sir William Alexander Craigie (August 13)",
+"Malefic Rookie",
+"Quiver Barnet",
+"The Last Artisan",
+"To be Left Alone",
+"Happy Birthday",
+"Hallelujah Gate",
+"Questions Making Trouble",
+"Crocs to Go",
+"Ditch the Sunset",
+"Lazy",
+"Caged Poems",
+"New House",
+"Snores, Doors and Jolts",
+"Accounting for My Trips (A — Z)",
+"Mariam",
+"Love Letters and Promenades",
+"My Rip Jean",
+"Six to Eight",
+"Catlick",
+"The Dot before the Hallo",
+"The Sleazy",
+"Lame in Dubai",
+"Our Love",
+"Twenty-Four",
+"Title"
 ];
 
 const drawText = (ctx, text, x, y, maxWidth) => {
@@ -78,7 +100,7 @@ const generateImages = async () => {
 
         // Save the image
         const buffer = canvas.toBuffer('image/png');
-        fs.writeFileSync(path.join(outputDir, `image_${i + 31}.png`), buffer);
+        fs.writeFileSync(path.join(outputDir, `image_${i + 1}.png`), buffer);
     }
 };
 
