2357b06d41e9be68da7a493d166e0e135e8ef408
DbRDYZmMRu <frithmangrove@gmail.com>
2025-03-25 13:35:52 +0100

    Update generate_images.js

--- generate_images.js
+++ generate_images.js
@@ -12,20 +12,7 @@ const titles = [
     "Goodnight Irene", "The Day Club", "Favourite Cardigan", "The Week", "Morning Pale", "Whiskers",
     "Dog-Eared Love", "Cubicle", "Lover", "Paper Planes", "Vinyl Perfect", "Pinchbeck Desire", "Hallway",
     "Those Things", "Grungy Ball", "The Absence Of Self", "Blind Reflection", "Ever Land", "Haiku. Why Cool!",
-    "With You", "What Could Have Been", "Zebra High", "Look What You Made Me Do", "Popcorn Tub", "Style",
-    "Rose Lens", "Clock Tock", "Far From The Early Y2K", "Plan Bs", "Dense Thrusting Shadows", "The Release",
-    "Crock Forte", "A Rainy Day", "Cows", "Yuck", "XXX", "Le Nippy Deux", "Fruit", "Numero Uno", "I", "Cut",
-    "Hue", "Pop", "Blacktop Garage", "Fait Accompli", "Cloud Aperture", "Pinky", "Screens", "Up To Eleven",
-    "1251", "Half Blue Pod", "The Dark", "XV", "XVI", "Brood X", "A Tonnage", "Meeting a Girl Is Like Swinging a Set",
-    "Hands", "It’s a New Day", "Earth Tweet", "Question Mark", "Im-Proach", "Hecatomb", "Velcro", "Docusoap",
-    "Over To", "Stool", "Cake", "Wonder Tincture", "Panache Of a Late Cheer", "August 19", "Errands", "We Met",
-    "Are You?", "Butt And Ash", "Every", "La Mia Innamorata", "Charly Cox", "Pesky 1989", "Tease", "Amadavat",
-    "Roast", "She’s The Trip", "What Choice", "Electric", "Metachrosis Buckle", "Taylor Swift", "Ready",
-    "Sign Of The Cornicle", "In My Place", "Ellipsis", "This Is Me Trying (Home)", "LVH Healing", "Tunnel",
-    "Mum", "Sock", "The Bubble Wrap", "Earworms", "I", "II", "Divan", "Bouchée à La Reine", "Again", "Two High",
-    "Graphic Grey Stencil", "Body Heat", "Her Wit Spittle", "Issue: What Remains?", "Taylor Do", "Poem 108",
-    "Late For The Revue", "Stodgy Pouffe", "Apostrophe", "The Re-Occurring Transition", "Roadwork Ahead",
-    "The Anon", "The wait (Journal entry)"
+    "With You", "What Could Have Been", "Zebra High", "Look What You Made Me Do"
 ];
 
 const drawText = (ctx, text, x, y, maxWidth) => {
