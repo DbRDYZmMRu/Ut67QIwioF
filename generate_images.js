a964aeafa88a5691174be2d407c09bf0c9af7c18
DbRDYZmMRu <frithmangrove@gmail.com>
2025-03-26 04:18:19 +0100

    Update generate_images.js

--- generate_images.js
+++ generate_images.js
@@ -7,7 +7,7 @@ registerFont('font/Architects_Daughter.ttf', { family: 'Architects Daughter' });
 
 const imageSize = 1080;
 const bgImagePath = 'https://raw.githubusercontent.com/DbRDYZmMRu/Ut67QIwioF/refs/heads/main/images/share/bg.jpg';
-const outputDir = path.join(__dirname, 'images', 'share', 'FHC', '12');
+const outputDir = path.join(__dirname, 'images', 'share', 'FHC', '13');
 const titles = [
 "Three In Corm",
 "Iniquity",
@@ -92,7 +92,7 @@ const generateImages = async () => {
         ctx.fillStyle = 'black';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'top';
-        ctx.fillText('FRITH HILTON COLLECTION XII', imageSize / 2, 10); // Corrected position
+        ctx.fillText('FRITH HILTON COLLECTION XIII', imageSize / 2, 10); // Corrected position
 
         // Save the image
         const buffer = canvas.toBuffer('image/png');
