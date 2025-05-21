893111c9932d40047f847152cf903bd5b3c95004
DbRDYZmMRu <frithmangrove@gmail.com>
2025-05-21 15:00:17 +0100

    Update upcomingPageAds.js

--- js/upcomingPageAds.js
+++ js/upcomingPageAds.js
@@ -1,5 +1,5 @@
 const adsTop = [
-  { img: '../images/ads/drinksprinter.gif', link: '#', displayTime: 9000 }, { img: '../images/ads/KylieCosmeticsHybrid_ii.gif', link: '#', displayTime: 9000 },
+  { img: '../images/ads/drinksprinter.gif', link: '#', displayTime: 9000 }, { img: '../images/ads/kyliecosmeticsHybridIII.gif', link: 'http://kyliecosmetics.com/', displayTime: 6000 },
   { img: '../images/ads/Crocs.gif', link: 'https://www.crocs.com/stories/come-as-you-are.html', displayTime: 5000 },
   { img: '../images/ads/Celsius.gif', link: '#', displayTime: 10000 },
   { img: '../images/ads/818.gif', link: '#', displayTime: 9000 },
@@ -11,7 +11,7 @@ const adsTop = [
 
 const adsBottom = [
   { img: '../images/ads/xokhloe.gif', link: 'https://www.ulta.com/p/xo-khloe-pimprod2049077?sku=2632566&fbclid=PAZXh0bgNhZW0CMTEAAaZVXM4Lkj92LaOM2GEyeMxVRAlahdOYqvDPGzl8R2-Jsf758_E9PY8hyC8_aem__mk4AnSGz28b8amcS9xKDg', displayTime: 20000 },
-  { img: '../images/ads/KylieCosmeticsHybrid_ii.gif', link: '#', displayTime: 9000 },
+  { img: '../images/ads/kyliecosmeticsHybridIII.gif', link: 'http://kyliecosmetics.com/', displayTime: 6000 },
   { img: '../images/ads/skims.gif', link: 'https://www.skims.com/', displayTime: 11000 },
   { img: '../images/ads/khy-wardrobe.gif', link: 'https://www.khy.com/pages/wardrobe?fbclid=PAY2xjawICrBJleHRuA2FlbQIxMQABpnTcHafMzC6bedRk_jGcKKLw_Qo60ipUANWhcdWC1TORtK4tF7n3YwA15w_aem__Z_2kS8T5xv2979yBkDWaw', displayTime: 10000 },
   { img: '../images/ads/cosmic.gif', link: 'https://www.ulta.com/p/cosmic-kylie-jenner-eau-de-parfum-pimprod2044205', displayTime: 7000 },
@@ -20,18 +20,32 @@ const adsBottom = [
   { img: '../images/ads/miumiu.gif', link: 'https://clicklinkin.bio/miumiu?fbclid=PAY2xjawIYGF9leHRuA2FlbQIxMQABpty8JWZkrJHYl04QkNw_-SfPVWMXQp0sHioNDNUgK1sUtfpB7w56WZEwjw_aem_rWOmS-mWjRVH4jeRmbTORQ', displayTime: 18000 },
 ];
 
+
+const adsMiddle = [
+  
+  { img: '../images/ads/kyliecosmeticsHybridI.gif', link: 'http://kyliecosmetics.com/', displayTime: 6500 },
+  { img: '../images/ads/kyliecosmeticsHybridII.gif', link: 'http://kyliecosmetics.com/', displayTime: 9000 },
+];
+
+
+
+
 let currentTopAdIndex = 0;
 let currentBottomAdIndex = 0;
+let currentMiddleAdIndex = 0;
 
 let topAdStartTime = Date.now();
 let bottomAdStartTime = Date.now();
+let middleAdStartTime = Date.now();
 
 function rotateAds() {
   const topAdContainer = document.getElementById('ad-top');
   const bottomAdContainer = document.getElementById('ad-bottom');
+  const middleAdContainer = document.getElementById('ad-middle');
   
   const topAd = adsTop[currentTopAdIndex];
   const bottomAd = adsBottom[currentBottomAdIndex];
+  const middleAd = adsMiddle[currentMiddleAdIndex];
   
   if (Date.now() - topAdStartTime >= topAd.displayTime) {
     topAdContainer.innerHTML = `<a href="${topAd.link}" target="_blank"><img src="${topAd.img}" alt="Advertisement" class="img-fluid"></a>`;
@@ -44,11 +58,19 @@ function rotateAds() {
     currentBottomAdIndex = (currentBottomAdIndex + 1) % adsBottom.length;
     bottomAdStartTime = Date.now();
   }
+  
+  if (Date.now() - middleAdStartTime >= middleAd.displayTime) {
+    middleAdContainer.innerHTML = `<a href="${middleAd.link}" target="_blank"><img src="${middleAd.img}" alt="Advertisement" class="img-fluid"></a>`;
+    currentMiddleAdIndex = (currentMiddleAdIndex + 1) % adsMiddle.length;
+    middleAdStartTime = Date.now();
+  }
 }
+
 
 const minimumDisplayTime = Math.min(
   ...adsTop.map(ad => ad.displayTime),
-  ...adsBottom.map(ad => ad.displayTime)
+  ...adsBottom.map(ad => ad.displayTime),
+  ...adsMiddle.map(ad => ad.displayTime)
 );
 
 setInterval(rotateAds, minimumDisplayTime);
