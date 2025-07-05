e1e3945046a1d3ade446bac45d7df1b74e72149f
DbRDYZmMRu <frithmangrove@gmail.com>
2025-07-05 04:13:48 +0100

    Update upcomingPageAds.js

--- js/upcomingPageAds.js
+++ js/upcomingPageAds.js
@@ -6,7 +6,7 @@ const adsTop = [
   { img: '../images/ads/kimsSecret.gif', link: 'https://shop.kims-secret.com, ', displayTime: 10000 },
   { img: '../images/ads/Rhode1.gif', link: 'https://www.rhodeskin.com', displayTime: 12000 },
   { img: '../images/ads/cocacola.gif', link: '#', displayTime: 30000 },
-  { img: '../images/ads/Personalday.gif', link: 'https://www.personalday.com/', displayTime: 10000 }, 
+  { img: '../images/ads/Personalday.gif', link: 'https://www.personalday.com/', displayTime: 10000 },
   { img: '../images/ads/RenneJuly2.gif', link: 'https://www.estellaco.co/products/dulce-vita-corset-micro-short-set', displayTime: 10000 }
   
 ];
@@ -18,13 +18,17 @@ const adsBottom = [
   { img: '../images/ads/khy-wardrobe.gif', link: 'https://www.khy.com/pages/wardrobe?fbclid=PAY2xjawICrBJleHRuA2FlbQIxMQABpnTcHafMzC6bedRk_jGcKKLw_Qo60ipUANWhcdWC1TORtK4tF7n3YwA15w_aem__Z_2kS8T5xv2979yBkDWaw', displayTime: 10000 },
   { img: '../images/ads/cosmic.gif', link: 'https://www.ulta.com/p/cosmic-kylie-jenner-eau-de-parfum-pimprod2044205', displayTime: 7000 },
   { img: '../images/ads/wildflower.gif', link: 'https://www.wildflowercases.com/en-ad', displayTime: 12000 },
+    { img: '../images/ads/GartoradeJuly5.gif', link: '#', displayTime: 9000 },
   { img: '../images/ads/kylieCosmeticsAd1.gif', link: 'https://kyliecosmetics.com/', displayTime: 20000 },
-  { img: '../images/ads/miumiu.gif', link: 'https://clicklinkin.bio/miumiu?fbclid=PAY2xjawIYGF9leHRuA2FlbQIxMQABpty8JWZkrJHYl04QkNw_-SfPVWMXQp0sHioNDNUgK1sUtfpB7w56WZEwjw_aem_rWOmS-mWjRVH4jeRmbTORQ', displayTime: 18000 },
+  { img: '../images/ads/KylieJuly4.gif', link: 'http://kyliecosmetics.com/', displayTime: 18000 },
 ];
 
 
 const adsMiddle = [
-  { img: '../images/ads/skimsJune10.gif', link: 'https://skims.com/en-ng/collections/coming-soon?utm_source=instagram-story&utm_medium=owned-social&utm_campaign=coming-soon&fbclid=PAZXh0bgNhZW0CMTEAAaei-K58mSObsPCBHWNbCpsqhKEjl9pdszWV0md-E0gMlNp3FEx49r5QiFe99A_aem_IweLTL5knnu1hmZ2eN4RGg', displayTime: 6500 },  
+    { img: '../images/ads/GartoradeJuly5.gif', link: '#', displayTime: 9000 },
+  { img: '../images/ads/skimsJune10.gif', link: 'https://skims.com/en-ng/collections/coming-soon?utm_source=instagram-story&utm_medium=owned-social&utm_campaign=coming-soon&fbclid=PAZXh0bgNhZW0CMTEAAaei-K58mSObsPCBHWNbCpsqhKEjl9pdszWV0md-E0gMlNp3FEx49r5QiFe99A_aem_IweLTL5knnu1hmZ2eN4RGg', displayTime: 6500 },
+  { img: '../images/ads/RhodeJuly5.gif', link: 'https://www.rhodeskin.com', displayTime: 9000 },
+  { img: '../images/ads/LVJuly5.gif', link: 'https://us.louisvuitton.com/eng-us/new/for-men/fall-winter-2025-show/_/N-t12jpbgi?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_MENSCAMPAIGNFW25_IG_WW_ENG_AW_20250703_20250713_MEN_RTW_MEN_FW25', displayTime: 9000 },
   { img: '../images/ads/SkimsAdJune6.gif', link: 'https://skims.com/en-ng/collections/skims-ultimate-edit?utm_source=ig&utm_medium=owned-social&utm_campaign=ultimate-edit&fbclid=PAQ0xDSwKv7GtleHRuA2FlbQIxMQABpzbyA74E5sCYgCUWUyyYVgROlPmgUwOfL2EGEikPL9D0Ztko7x0lYfYaUKtD_aem_iWqvZwXXJEHjrzIrE3qX5Q', displayTime: 6500 },
   { img: '../images/ads/slipperySoapAd1.gif', link: 'https://www.slipperysoap.com/', displayTime: 6500 },
   { img: '../images/ads/KhyXDilaraAd.gif', link: 'https://khy.com/', displayTime: 6500 },
