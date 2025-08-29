558002fe0fe16ca1da293eea3226de8accfdff90
DbRDYZmMRu <frithmangrove@gmail.com>
2025-08-29 10:24:48 +0100

    Create ad-data-ii.js

---
+++ js/ad-data-ii.js
@@ -0,0 +1,52 @@
+// ad-data-ii.js
+window.adData = [
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_6.mp4',
+  duration: 58,
+  cta: {
+    message: `The closest thing I‘ ve seen to a representation of a painting on a digital medium.“<br><br>From painting to film, Basim Magdy’ s art journey has always involved pushing boundaries. Hear his story - and explore the new possibilities of art today✨.`,
+    link: '#',
+    buttonText: 'Explore Now'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_5.mp4',
+  duration: 18,
+  cta: {
+    message: `#cokepartner No matter the decade, some things never go out of style. <br> <span style="color: red;">Iconic</span> in any era.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_2.mp4',
+  duration: 54,
+  cta: {
+    message: `This one is close to my heart: our Coco Ring now gives back. 💍 Through our partnership with @TheoGraceOfficial and @AnimalHaven, 10% of proceeds will support their incredible work rescuing and rehoming animals in need. 🐾 A 💍 that not just looks good but does good too. 🩵`,
+    link: 'https://www.theograce.com/',
+    buttonText: 'Get Yours!'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_3.mp4',
+  duration: 14,
+  cta: {
+    message: `When your rival is at the door, who will you become ?<br><br>This is NFL Nike Rivalries.<br>Show them who you are.<br>`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_4.mp4',
+  duration: 14,
+  cta: {
+    message: `When your rival is at the door, who will you become ?<br><br>This is NFL Nike Rivalries.<br>Show them who you are.<br>`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+}];
