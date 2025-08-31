c28ce804a6feb5295ff78bc88a8cee70eb9a1956
DbRDYZmMRu <frithmangrove@gmail.com>
2025-08-31 06:41:39 +0100

    Create ad-data-iii.js

---
+++ js/ad-data-iii.js
@@ -0,0 +1,80 @@
+// ad-data-iii.js
+window.adData = [
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_1.mp4',
+  duration: 42,
+  cta: {
+    message: `Add Log Off, Luxplore, and OOOasis to your vocabulary - AutoCamp is redefining the way you vacay.<br><br>We’re the nature escape you need, with the boutique experience you want - explore AutoCamp’s iconic outdoor locations all season long.`,
+    link: 'https://autocamp.com/',
+    buttonText: 'Explore Now'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_5.mp4',
+  duration: 18,
+  cta: {
+    message: `#cokepartner No matter the decade, some things never go out of style. <br> <span style="color: red;">Iconic</span> in any era.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_8.mp4',
+  duration: 31,
+  cta: {
+    message: `A little sparkle goes a long way ✨ <br> Don’ t forget — 10% of every Coco Ring purchase supports @animalhaven and the incredible work they do to rescue and rehome animals in need🐾🤍<br><br>
+  
+  Because jewelry should shine brighter when it gives back!💍<br>
+  
+  Nicky Hilton`,
+    link: 'https://www.theograce.com/',
+    buttonText: 'Get Yours!'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_9.mp4',
+  duration: 27,
+  cta: {
+    message: `Welcome to the All Conditions Racing Department. <br><br>OUTRUN EVERYTHING.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_7.mp4',
+  duration: 13,
+  cta: {
+    message: `Rouge radiance. Invoking the fierce spirit of Louis Vuitton, @patmcgrathreal crafts a graphic statement on model Awar, blending the heritage-inspired shades 896 Monogram Rouge and 854 Rouge Louis of the LV Rouge lipstick with grounding earth tones and glistening gold accents from the LV Ombres palette in 951 Force of Nature — an audacious declaration forged in the primal energy of nature.`,
+    link: 'https://us.louisvuitton.com/eng-us/stories//la-beaute-louis-vuitton?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_BEAUTY_IG_US_ENGAW_20250819_20250930_WOM_BTY',
+    buttonText: 'Get the Look'
+  },
+}, 
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_10.gif',
+  duration: 8,
+  cta: {
+    message: `Rouge radiance. Invoking the fierce spirit of Louis Vuitton, @patmcgrathreal crafts a graphic statement on model Awar, blending the heritage-inspired shades 896 Monogram Rouge and 854 Rouge Louis of the LV Rouge lipstick with grounding earth tones and glistening gold accents from the LV Ombres palette in 951 Force of Nature — an audacious declaration forged in the primal energy of nature.`,
+    link: 'https://us.louisvuitton.com/eng-us/stories//la-beaute-louis-vuitton?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_BEAUTY_IG_US_ENGAW_20250819_20250930_WOM_BTY',
+    buttonText: 'Get the Look'
+  },
+}, 
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_11.gif',
+  duration: 12,
+  cta: {
+    message: `Just Dropped: New Essentials. An all-new collection of must-haves—including the sexiest Milky Sheer intimates and comfiest Cotton Jersey basics.<br><br>
+
+Kim Kardashian wears the Milky Sheer String Bikini, size S, and Cotton Jersey Long Sleeve T-Shirt, size XS, in Soot.`,
+    link: 'https://skims.com/en-ng?utm_source=instagram&utm_medium=owned-social&utm_campaign=launch&fbclid=PAZXh0bgNhZW0CMTEAAackHH0Tj9aRQ-v15Dnhuev4Kir7wbuZS5tG1m7fYGNYEIlOBpXIbv6q594_Ow_aem_jw57_LmBZV5TqozuWkDaSg',
+    buttonText: 'Shop SKIMS'
+  },
+}  
+
+];
