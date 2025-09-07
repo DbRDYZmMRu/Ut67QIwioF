29e6a7e80d18791019c1668721f05fbc882573ec
DbRDYZmMRu <frithmangrove@gmail.com>
2025-09-07 11:51:08 +0100

    Create ad-data-iv.js

---
+++ js/ad-data-iv.js
@@ -0,0 +1,119 @@
+// ad-data-iv.js
+window.adData = [
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_21.mp4',
+  duration: 11,
+  cta: {
+    message: `our spooookiest drop of the season 👻🕸️ coming soon…...`,
+    link: 'https://www.wildflowercases.com/en-ad',
+    buttonText: 'Get Yours'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_13.mp4',
+  duration: 15,
+  cta: {
+    message: `He is the Forever King.<br><br>
+
+The LeBron XXIII arrives soon. Prepare to behold the greatness.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_5.mp4',
+  duration: 6,
+  cta: {
+    message: `#cokepartner No matter the decade, some things never go out of style. <br> <span style="color: red;">Iconic</span> in any era.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_2.mp4',
+  duration: 25,
+  cta: {
+    message: `A little sparkle goes a long way ✨ <br> Don’ t forget — 10% of every Coco Ring purchase supports @animalhaven and the incredible work they do to rescue and rehome animals in need🐾🤍<br><br>
+  
+  Because jewelry should shine brighter when it gives back!💍<br>
+  
+  Nicky Hilton`,
+    link: 'https://www.theograce.com/',
+    buttonText: 'Get Yours!'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_15.mp4',
+  duration: 30,
+  cta: {
+    message: `Why risk it? Because you can. #JustDoIt...`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_7.mp4',
+  duration: 8,
+  cta: {
+    message: `Rouge radiance.Invoking the fierce spirit of Louis Vuitton. < br > <br>
+    Dame Pat McGrath, crafts a graphic statement on model Awar, blending the heritage-inspired shades 896 Monogram Rouge and 854 Rouge Louis of the LV Rouge lipstick with grounding earth tones and glistening gold accents from the LV Ombres palette in 951 Force of Nature — an audacious declaration forged in the primal energy of nature.`,
+    link: 'https://us.louisvuitton.com/eng-us/stories//la-beaute-louis-vuitton?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_BEAUTY_IG_US_ENGAW_20250819_20250930_WOM_BTY',
+    buttonText: 'Get the Look'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_22.mp4',
+  duration: 15, 
+  cta: {
+    message: `In this episode of Louis Vuitton [Extended], Loïc Prigent shares an inspiring exchange with Tahar Rahim — internationally acclaimed, César Award winning actor and Friend of the House. Together, they explore his instinctive approach to selecting roles, his pursuit of perfection, and his extraordinary ability to transform for each character.`,
+    link: 'https://smartlinks.audiomeans.fr/l/louis-vuitton-extended-87c07a4e/tahar-rahim-on-cinema-pushing-boundaries-and-his-ability-to-transform-d5182bbf',
+    buttonText: 'Listen to the Podcast'
+  },
+},
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_16.gif',
+  duration: 5,
+  cta: {
+    message: `Tailored pants for elevated day-to-night wear. Discover our new, mid-weight knit bottoms with compressive stretch`,
+    link: 'https://skims.com/en-ng/collections/the-essential-collection',
+    buttonText: 'Shop SKIMS ESSENTIAL COLLECTION'
+  },
+},
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_19.gif',
+  duration: 5,
+  cta: {
+    message: `Elevated layers, iconic pieces—everything you need for fall is here`,
+    link: 'https://skims.com/en-ng/collections/fall-shop?utm_source=instagram-story&utm_medium=ownedsocial&utm_campaign=fallshop&fbclid=PAZXh0bgNhZW0CMTEAAaf0KQwM97wbX-CgFAx9gasPd7eTbgoC8QQNQLhsp5ggqqpj8vwSyz6qH7GqlQ_aem_VIiPYw5RooMe0RQ6t-FiGg',
+    buttonText: 'Shop SKIMS FALL WARDROBE'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_23.mp4',
+  duration: 26,
+  cta: {
+    message: `Helia Golbinrad captures the essence of ETERNAL ROOTS.<br><br> Inspired by the balance between freshness and depth, just like moments spent surrounded by nature and open air.`,
+    link: 'https://orebella.com',
+    buttonText: 'Shop the full ETERNAL ROOTS collection now'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_14.mp4',
+  duration: 4,
+  cta: {
+    message: `Add Log Off to your vocabulary - AutoCamp is redefining the way you vacay.<br><br>We’re the nature escape you need, with the boutique experience you want - explore AutoCamp’s iconic outdoor locations all season long.`,
+    link: 'https://autocamp.com/',
+    buttonText: 'Explore Now'
+  }
+}, ];
