6460eb73ee93f6a1f49c4aa5816d4293528a095a
DbRDYZmMRu <frithmangrove@gmail.com>
2025-09-09 07:48:38 +0100

    Update ad-data-ii.js

--- js/ad-data-ii.js
+++ js/ad-data-ii.js
@@ -1,9 +1,9 @@
-// ad-data-ii.js
+// ad-data.js
 window.adData = [
 {
   type: 'video',
   url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_6.mp4',
-  duration: 58,
+  duration: 15,
   cta: {
     message: `The closest thing I‘ ve seen to a representation of a painting on a digital medium.“<br><br>From painting to film, Basim Magdy’ s art journey has always involved pushing boundaries. Hear his story - and explore the new possibilities of art today✨.`,
     link: '#',
@@ -12,8 +12,30 @@ window.adData = [
 },
 {
   type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_21.mp4',
+  duration: 11,
+  cta: {
+    message: `our spooookiest drop of the season 👻🕸️ coming soon…...`,
+    link: 'https://www.wildflowercases.com/en-ad',
+    buttonText: 'Get Yours'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_13.mp4',
+  duration: 15,
+  cta: {
+    message: `He is the Forever King.<br><br>
+
+The LeBron XXIII arrives soon. Prepare to behold the greatness.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
   url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_5.mp4',
-  duration: 18,
+  duration: 6,
   cta: {
     message: `#cokepartner No matter the decade, some things never go out of style. <br> <span style="color: red;">Iconic</span> in any era.`,
     link: '#',
@@ -23,7 +45,7 @@ window.adData = [
 {
   type: 'video',
   url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_2.mp4',
-  duration: 54,
+  duration: 30,
   cta: {
     message: `This one is close to my heart: our Coco Ring now gives back. 💍 Through our partnership with @TheoGraceOfficial and @AnimalHaven, 10% of proceeds will support their incredible work rescuing and rehoming animals in need. 🐾 A 💍 that not just looks good but does good too. 🩵`,
     link: 'https://www.theograce.com/',
@@ -32,20 +54,52 @@ window.adData = [
 },
 {
   type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_3.mp4',
-  duration: 14,
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_22.mp4',
+  duration: 15, 
   cta: {
-    message: `When your rival is at the door, who will you become ?<br><br>This is NFL Nike Rivalries.<br>Show them who you are.<br>`,
+    message: `In this episode of Louis Vuitton [Extended], Loïc Prigent shares an inspiring exchange with Tahar Rahim — internationally acclaimed, César Award winning actor and Friend of the House. Together, they explore his instinctive approach to selecting roles, his pursuit of perfection, and his extraordinary ability to transform for each character.`,
+    link: 'https://smartlinks.audiomeans.fr/l/louis-vuitton-extended-87c07a4e/tahar-rahim-on-cinema-pushing-boundaries-and-his-ability-to-transform-d5182bbf',
+    buttonText: 'Listen to the Podcast'
+  },
+},
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_16.gif',
+  duration: 5,
+  cta: {
+    message: `Tailored pants for elevated day-to-night wear. Discover our new, mid-weight knit bottoms with compressive stretch`,
+    link: 'https://skims.com/en-ng/collections/the-essential-collection',
+    buttonText: 'Shop SKIMS ESSENTIAL COLLECTION'
+  },
+},
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_19.gif',
+  duration: 5,
+  cta: {
+    message: `Elevated layers, iconic pieces—everything you need for fall is here`,
+    link: 'https://skims.com/en-ng/collections/fall-shop?utm_source=instagram-story&utm_medium=ownedsocial&utm_campaign=fallshop&fbclid=PAZXh0bgNhZW0CMTEAAaf0KQwM97wbX-CgFAx9gasPd7eTbgoC8QQNQLhsp5ggqqpj8vwSyz6qH7GqlQ_aem_VIiPYw5RooMe0RQ6t-FiGg',
+    buttonText: 'Shop SKIMS FALL WARDROBE'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_24.mp4',
+  duration: 24,
+  cta: {
+    message: `Opposites attack.<br><br>
+
+The next era of tennis belongs to those on the offense.`,
     link: '#',
     buttonText: 'Learn More'
   }
 },
 {
   type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/aug/ad_4.mp4',
-  duration: 14,
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_25.mp4',
+  duration: 16,
   cta: {
-    message: `When your rival is at the door, who will you become ?<br><br>This is NFL Nike Rivalries.<br>Show them who you are.<br>`,
+    message: `Turn it up. Light it up. Unleash the beat anywhere with the Sound Tower. Portable, splashproof, and up to 18 hours of playtime. 🪩`,
     link: '#',
     buttonText: 'Learn More'
   }
