2e912172cae6ace005b3530a9855427388b0a394
DbRDYZmMRu <frithmangrove@gmail.com>
2025-10-06 02:28:19 +0100

    Update ad-data-ii.js

--- js/ad-data-ii.js
+++ js/ad-data-ii.js
@@ -1,184 +1,195 @@
 // ad-data-ii.js
 window.adData = [
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_6.mp4',
-  duration: 32,
-  cta: {
-    message: `Just the two of us, wandering where the leaves lead.⁠<br><br>Your adventure starts at the click of the button.`,
-    link: 'https://autocamp.com/',
-    buttonText: 'Adventure!'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_26.mp4',
-  duration: 15,
-  cta: {
-    message: `Built by science. Certified by sweat.<br><br>Gatorade`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_7.mp4',
-  duration: 92,
-  cta: {
-    message: `A screen that moves with you, so life never pauses.<br> With an integrated kick-stand and a built-in battery, set it down anywhere. While Samsung Art Store and Vision AI curates your world:<br>art that transforms your space, entertainment that matches your mood—all without you having to ask. <br> From corner to corner, it goes where you go. Every control feels effortless.<br><br> Move it, Kick it, Feel it, Play it. `,
-    link: '#',
-    buttonText: 'Explore Now'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_29.mp4',
-  duration: 15,
-  cta: {
-    message: `Days like this write themselves into our memory, forever.<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter I by <b>Katherine Goguen</b>`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_31.mp4',
-  duration: 15,
-  cta: {
-    message: `The making of campfire stories in real time.
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_6.mp4',
+    duration: 32,
+    cta: {
+      message: `Just the two of us, wandering where the leaves lead.⁠<br><br>Your adventure starts at the click of the button.`,
+      link: 'https://autocamp.com/',
+      buttonText: 'Adventure!'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_26.mp4',
+    duration: 15,
+    cta: {
+      message: `Built by science. Certified by sweat.<br><br>Gatorade`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_7.mp4',
+    duration: 92,
+    cta: {
+      message: `A screen that moves with you, so life never pauses.<br> With an integrated kick-stand and a built-in battery, set it down anywhere. While Samsung Art Store and Vision AI curates your world:<br>art that transforms your space, entertainment that matches your mood—all without you having to ask. <br> From corner to corner, it goes where you go. Every control feels effortless.<br><br> Move it, Kick it, Feel it, Play it. `,
+      link: '#',
+      buttonText: 'Explore Now'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_29.mp4',
+    duration: 15,
+    cta: {
+      message: `Days like this write themselves into our memory, forever.<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter I by <b>Katherine Goguen</b>`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_31.mp4',
+    duration: 15,
+    cta: {
+      message: `The making of campfire stories in real time.
 <br><br> Coke Films: The Last Sips of Summer<br><br>Chapter II by <b>Orland Jensen</b>`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_32.mp4',
-  duration: 20,
-  cta: {
-    message: `See you, summer. We’ll replay these days until we meet again. <br><br> .Coke Films: The Last Sips of Summer<br><br>Chapter III by <b>Q Guajardo </b>`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_2.mp4',
-  duration: 4,
-  cta: {
-    message: `Chuck Staples has a nice ring to it. Stay tuned. <br><br> <b>Converse</b>`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'image',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_3.gif',
-  duration: 6,
-  cta: {
-    message: `Showgirling`,
-    link: '#',
-    buttonText: 'Learn More'
+      link: '#',
+      buttonText: 'Learn More'
+    }
   },
-},
-
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_45.mp4',
-  duration: 14,
-  cta: {
-    message: `The Delina heart bracelet, keep them close always. 👧🏼✨`,
-    link: 'https://www.theograce.com/',
-    buttonText: 'Get Yours!'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_9.mp4',
-  duration: 45,
-  cta: {
-    message: `Home is where I want to be — Nicolas Ghesquière <br><br>Louis Vuitton Runway Reflections<br>Stay tuned for Chapter 3 of Runway Reflections — presented by Tuba. <br>Exclusively on YouTube, October 29th.`,
-    link: '#',
-    buttonText: 'Learn More'
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_32.mp4',
+    duration: 20,
+    cta: {
+      message: `See you, summer. We’ll replay these days until we meet again. <br><br> .Coke Films: The Last Sips of Summer<br><br>Chapter III by <b>Q Guajardo </b>`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
   },
-},
-{
-  type: 'image',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_40.gif',
-  duration: 10,
-  cta: {
-    message: `SKIMS Ultimate: the most natural shape, lift and fullness you’ve ever experienced from a push-up solution.`,
-    link: 'https://skims.com',
-    buttonText: 'Shop SKIMS'
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_2.mp4',
+    duration: 4,
+    cta: {
+      message: `Chuck Staples has a nice ring to it. Stay tuned. <br><br> <b>Converse</b>`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
   },
-},
-{
-  type: 'image',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_39.gif',
-  duration: 10,
-  cta: {
-    message: `Luxury Knits. Meet fall in a brand new sweater collection of the softest premium cashmere and coziest layers in timeless silhouettes designed to be worn forever.`,
-    link: 'https://skims.com',
-    buttonText: 'Shop SKIMS'
+  {
+    type: 'image',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_3.gif',
+    duration: 6,
+    cta: {
+      message: `Showgirling`,
+      link: '#',
+      buttonText: 'Learn More'
+    },
   },
-},
-{
-  type: 'image',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_35.gif',
-  duration: 8,
-  cta: {
-    message: `Kylie Cosmetics dessert in a mist.  <br>🍫 sweet éclair 🌸 vanilla`,
-    link: 'https://kyliecosmetics.com/',
-    buttonText: 'Shop'
+  
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_45.mp4',
+    duration: 14,
+    cta: {
+      message: `The Delina heart bracelet, keep them close always. 👧🏼✨`,
+      link: 'https://www.theograce.com/',
+      buttonText: 'Get Yours!'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_9.mp4',
+    duration: 45,
+    cta: {
+      message: `Home is where I want to be — Nicolas Ghesquière <br><br>Louis Vuitton Runway Reflections<br>Stay tuned for Chapter 3 of Runway Reflections — presented by Tuba. <br>Exclusively on YouTube, October 29th.`,
+      link: '#',
+      buttonText: 'Learn More'
+    },
+  },
+  {
+    type: 'image',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_40.gif',
+    duration: 10,
+    cta: {
+      message: `SKIMS Ultimate: the most natural shape, lift and fullness you’ve ever experienced from a push-up solution.`,
+      link: 'https://skims.com',
+      buttonText: 'Shop SKIMS'
+    },
+  },
+  {
+    type: 'image',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_11.gif',
+    duration: 10,
+    cta: {
+      message: `SKIMS Fleece: redefining cold weather classics.<br><br>Melanie wears the Teddy Boxy Zip Up Jacket, size S, in Umber, and Essential High-Waisted Leggings, size S, in Obsidian.`,
+      link: 'https://skims.com',
+      buttonText: 'Shop SKIMS'
+    },
+  },
+  {
+    type: 'image',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_35.gif',
+    duration: 8,
+    cta: {
+      message: `Kylie Cosmetics dessert in a mist.  <br>🍫 sweet éclair 🌸 vanilla`,
+      link: 'https://kyliecosmetics.com/',
+      buttonText: 'Shop'
+    }
+  },
+  {
+    type: 'image',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_12.gif',
+    duration: 8,
+    cta: {
+      message: `Kylie wearing the Short Trench Coat in black.`,
+      link: 'https://khy.com/',
+      buttonText: 'Shop'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_1.mp4',
+    duration: 32,
+    cta: {
+      message: `Emotional Support Lip Balm <br><br> Meet the most hydrating lip balm you've ever had.<br>Finally, lip barrier care that hydrates & conditions with real skincare ingredients in a non-sticky, buttery smooth balm—all in an acne-safe formula so it won’t break you out.`,
+      link: 'https://www.personalday.com/collections/shop-all/products/emotional-support-lip-balm?fbclid=PAZXh0bgNhZW0CMTEAAadS-1QYIqX0_ITJw0M2d0o9gf6UJ3o78Ixs4Iem_2CGQ6znICCECrWuvAEdfw_aem_v-ufJ-YRIJ61cw6Ahlhzqw',
+      buttonText: 'Shop Personal Day '
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_47.mp4',
+    duration: 46,
+    cta: {
+      message: `Designed to sculpt. Engineered to perform.<br><br>A new brand for those who refuse to compromise.`,
+      link: 'https://skims.com/en-ng/collections/nikeskims',
+      buttonText: 'Shop'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_10.mp4',
+    duration: 33,
+    cta: {
+      message: `The 40 k Club.Only One Player. <br>The LeBron XXIII‘ Uncharted’ is more than a milestone.It’ s about pushing the boundaries of greatness.<br>Arrived in SNKRS in select locations 10.3.`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_4.mp4',
+    duration: 25,
+    cta: {
+      message: `This is more than a prototype. <br>This is an all-new sensation in running.<br>Be the first to experience zero gravity running with the limited edition Nike Vomero Premium “Proto” dropped October 2nd.`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
+  },
+  {
+    type: 'video',
+    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_8.mp4',
+    duration: 39,
+    cta: {
+      message: `Ever wondered who was behind the hotdog with no sausage ? 🌭<br>Every fake gets found out eventually.<br>Not all QLED are equal.Choose the real QLED.`,
+      link: '#',
+      buttonText: 'Learn More'
+    }
   }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_1.mp4',
-  duration: 32,
-  cta: {
-    message: `Emotional Support Lip Balm <br><br> Meet the most hydrating lip balm you've ever had.<br>Finally, lip barrier care that hydrates & conditions with real skincare ingredients in a non-sticky, buttery smooth balm—all in an acne-safe formula so it won’t break you out.`,
-    link: 'https://www.personalday.com/collections/shop-all/products/emotional-support-lip-balm?fbclid=PAZXh0bgNhZW0CMTEAAadS-1QYIqX0_ITJw0M2d0o9gf6UJ3o78Ixs4Iem_2CGQ6znICCECrWuvAEdfw_aem_v-ufJ-YRIJ61cw6Ahlhzqw',
-    buttonText: 'Shop Personal Day '
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_47.mp4',
-  duration: 46,
-  cta: {
-    message: `Designed to sculpt. Engineered to perform.<br><br>A new brand for those who refuse to compromise.`,
-    link: 'https://skims.com/en-ng/collections/nikeskims',
-    buttonText: 'Shop'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_10.mp4',
-  duration: 33,
-  cta: {
-    message: `The 40 k Club.Only One Player. <br>The LeBron XXIII‘ Uncharted’ is more than a milestone.It’ s about pushing the boundaries of greatness.<br>Arrived in SNKRS in select locations 10.3.`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_4.mp4',
-  duration: 25,
-  cta: {
-    message: `This is more than a prototype. <br>This is an all-new sensation in running.<br>Be the first to experience zero gravity running with the limited edition Nike Vomero Premium “Proto” dropped October 2nd.`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-},
-{
-  type: 'video',
-  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_8.mp4',
-  duration: 39,
-  cta: {
-    message: `Ever wondered who was behind the hotdog with no sausage ? 🌭<br>Every fake gets found out eventually.<br>Not all QLED are equal.Choose the real QLED.`,
-    link: '#',
-    buttonText: 'Learn More'
-  }
-}];
+];
