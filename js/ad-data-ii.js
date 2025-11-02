cfa3d322047bb91414068a559e2ca3091290f05e
DbRDYZmMRu <frithmangrove@gmail.com>
2025-11-02 07:58:41 +0100

    Update ad-data-ii.js

--- js/ad-data-ii.js
+++ js/ad-data-ii.js
@@ -1,215 +1,225 @@
 // ad-data-ii.js
 window.adData = [
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_6.mp4',
-    duration: 32,
-    cta: {
-      message: `Just the two of us, wandering where the leaves lead.⁠<br><br>Your adventure starts at the click of the button.`,
-      link: 'https://autocamp.com/',
-      buttonText: 'Adventure!'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_1.mp4',
-    duration: 21,
-    cta: {
-      message: `MONSE evening wear, where deconstruction meets elegance.`,
-      link: 'https://monse.com/?fbclid=PAZXh0bgNhZW0CMTEAAafX2pm3FBcgDWNqZ0oXoEbOqQ6rbphbFmpxZ5ZZC_ZFfIe5K6mqT9Z3hrXs0g_aem_60X27azZqG6DEoNJEMYEIg',
-      buttonText: 'Shop MONSE!'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_5.mp4',
-    duration: 15,
-    cta: {
-      message: `Code red. Something’s coming.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_10.mp4',
-    duration: 60,
-    cta: {
-      message: `Every fake performance has an ending.<br>
-Not all QLED are equal.<br><br> Choose the real QLED.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_29.mp4',
-    duration: 40,
-    cta: {
-      message: `Days like this write themselves into our memory, forever.<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter I by <b>Katherine Goguen</b>`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_31.mp4',
-    duration: 40,
-    cta: {
-      message: `The making of campfire stories in real time.
-<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter II by <b>Orland Jensen</b>`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_32.mp4',
-    duration: 50,
-    cta: {
-      message: `See you, summer. We’ll replay these days until we meet again. <br><br> .Coke Films: The Last Sips of Summer<br><br>Chapter III by <b>Q Guajardo </b>`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_3.mp4',
-    duration: 31,
-    cta: {
-      message: `Five creators brought their inspirations to life on Chucks. Use their designs to customize your own on Converse.com`,
-      link: 'https://www.converse.com/c/custom',
-      buttonText: 'Design Your Own Custom Shoes'
-    }
-  },
-  {
-    type: 'image',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_3.gif',
-    duration: 6,
-    cta: {
-      message: `All we need 📷`,
-      link: '#',
-      buttonText: 'Learn More'
-    },
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_4.mp4',
-    duration: 15,
-    cta: {
-      message: `Our Halloween Sale is here. Enjoy 25% OFF all jewelry and add a little magic to your look. 🪄 🎃 👻`,
-      link: 'https://www.theograce.com/',
-      buttonText: 'Shop TheoGrace!'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_6.mp4',
-    duration: 30,
-    cta: {
-      message: `Runway Reflections Chapter 3 — An ode to the home as a sanctuary of living.<br>Uncover backstage revelations at Nicolas Ghesquiere’s latest presentation unveiling with Tuba Avalon. <br><br>Watch the full film exclusively on YouTube.`,
-      link: 'https://youtu.be/ZRpHECexsiY?si=uqWT-rVmWFZcxdsY',
-      buttonText: 'Watch Documentary'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_2.mp4',
-    duration: 15,
-    cta: {
-      message: `Spring-Summer 2026 Pre-Collection. <br>An effortless elegance emerges with Friend of the House Jude Bellingham, embodying relaxed silhouettes that exemplify the modern Men's vision from Pharrell — underscoring the country pursuits through a tailored jacket and technical tweed-woven puffer.`,
-      link: 'https://us.louisvuitton.com/eng-us/new/for-men/spring-summer-2026-pre-collection/_/N-t1o8k6nm?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_MENS_PRECO_SS26_IG_WW_ENG_AW_20251024_20251102_MEN_RTW_MEN_PRECOLL_SS26',
-      buttonText: 'Shop the Pre-Collection'
-    },
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_14.mp4',
-    duration: 28,
-    cta: {
-      message: `Coming October 16: <br>The Ultimate Body. Bigger butt, rounder hips, smaller waist, instant boob job—get ready to have it all with our most iconic solutions yet.`,
-      link: 'https://skims.com',
-      buttonText: 'Shop SKIMS'
-    },
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_13.mp4',
-    duration: 88,
-    cta: {
-      message: `Just Dropped: <br>The Ultimate Bush. With our daring new Faux Hair Panty, your carpet can be whatever color you want it to be.`,
-      link: 'https://skims.com',
-      buttonText: 'Shop SKIMS'
-    },
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_21.mp4',
-    duration: 8,
-    cta: {
-      message: `No luck needed.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_1.mp4',
-    duration: 32,
-    cta: {
-      message: `Emotional Support Lip Balm <br><br> Meet the most hydrating lip balm you've ever had.<br>Finally, lip barrier care that hydrates & conditions with real skincare ingredients in a non-sticky, buttery smooth balm—all in an acne-safe formula so it won’t break you out.`,
-      link: 'https://www.personalday.com/collections/shop-all/products/emotional-support-lip-balm?fbclid=PAZXh0bgNhZW0CMTEAAadS-1QYIqX0_ITJw0M2d0o9gf6UJ3o78Ixs4Iem_2CGQ6znICCECrWuvAEdfw_aem_v-ufJ-YRIJ61cw6Ahlhzqw',
-      buttonText: 'Shop Personal Day '
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_47.mp4',
-    duration: 46,
-    cta: {
-      message: `Designed to sculpt. Engineered to perform.<br><br>A new brand for those who refuse to compromise.`,
-      link: 'https://skims.com/en-ng/collections/nikeskims',
-      buttonText: 'Shop'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_19.mp4',
-    duration: 15,
-    cta: {
-      message: `Don’t tell Saquon Barkley to stay grounded. He knows the sky is the limit.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_7.mp4',
-    duration: 30,
-    cta: {
-      message: `You can’t write this stuff. But LA just did again.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_25.mp4',
-    duration: 14,
-    cta: {
-      message: `There is no finish line when it comes to innovating to serve athletes.<br><br>Introducing four game - changing Nike innovations designed to transform what’ s possible.<br><br>Nike Mind<br>Nike Air Milano<br>Nike Aero - FIT<br>Nike Amplify<br>`,
-      link: 'https://empli.fi/nike/',
-      buttonText: 'Learn More'
-    }
-  },
-  {
-    type: 'video',
-    url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_9.mp4',
-    duration: 54,
-    cta: {
-      message: `Like a boss, doing less means more at your home with Samsung AI Home.`,
-      link: '#',
-      buttonText: 'Learn More'
-    }
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_6.mp4',
+  duration: 32,
+  cta: {
+    message: `Just the two of us, wandering where the leaves lead.⁠<br><br>Your adventure starts at the click of the button.`,
+    link: 'https://autocamp.com/',
+    buttonText: 'Adventure!'
   }
-];
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_1.mp4',
+  duration: 21,
+  cta: {
+    message: `MONSE evening wear, where deconstruction meets elegance.`,
+    link: 'https://monse.com/?fbclid=PAZXh0bgNhZW0CMTEAAafX2pm3FBcgDWNqZ0oXoEbOqQ6rbphbFmpxZ5ZZC_ZFfIe5K6mqT9Z3hrXs0g_aem_60X27azZqG6DEoNJEMYEIg',
+    buttonText: 'Shop MONSE!'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_5.mp4',
+  duration: 15,
+  cta: {
+    message: `Code red. Something’s coming.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_10.mp4',
+  duration: 60,
+  cta: {
+    message: `Every fake performance has an ending.<br>
+Not all QLED are equal.<br><br> Choose the real QLED.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_29.mp4',
+  duration: 40,
+  cta: {
+    message: `Days like this write themselves into our memory, forever.<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter I by <b>Katherine Goguen</b>`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_31.mp4',
+  duration: 40,
+  cta: {
+    message: `The making of campfire stories in real time.
+<br><br> Coke Films: The Last Sips of Summer<br><br>Chapter II by <b>Orland Jensen</b>`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_32.mp4',
+  duration: 50,
+  cta: {
+    message: `See you, summer. We’ll replay these days until we meet again. <br><br> .Coke Films: The Last Sips of Summer<br><br>Chapter III by <b>Q Guajardo </b>`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_3.mp4',
+  duration: 31,
+  cta: {
+    message: `Five creators brought their inspirations to life on Chucks. Use their designs to customize your own on Converse.com`,
+    link: 'https://www.converse.com/c/custom',
+    buttonText: 'Design Your Own Custom Shoes'
+  }
+},
+{
+  type: 'image',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_3.gif',
+  duration: 6,
+  cta: {
+    message: `All we need 📷`,
+    link: '#',
+    buttonText: 'Learn More'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_4.mp4',
+  duration: 15,
+  cta: {
+    message: `Our Halloween Sale is here. Enjoy 25% OFF all jewelry and add a little magic to your look. 🪄 🎃 👻`,
+    link: 'https://www.theograce.com/',
+    buttonText: 'Shop TheoGrace!'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_6.mp4',
+  duration: 30,
+  cta: {
+    message: `Runway Reflections Chapter 3 — An ode to the home as a sanctuary of living.<br>Uncover backstage revelations at Nicolas Ghesquiere’s latest presentation unveiling with Tuba Avalon. <br><br>Watch the full film exclusively on YouTube.`,
+    link: 'https://youtu.be/ZRpHECexsiY?si=uqWT-rVmWFZcxdsY',
+    buttonText: 'Watch Documentary'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_2.mp4',
+  duration: 15,
+  cta: {
+    message: `Spring-Summer 2026 Pre-Collection. <br>An effortless elegance emerges with Friend of the House Jude Bellingham, embodying relaxed silhouettes that exemplify the modern Men's vision from Pharrell — underscoring the country pursuits through a tailored jacket and technical tweed-woven puffer.`,
+    link: 'https://us.louisvuitton.com/eng-us/new/for-men/spring-summer-2026-pre-collection/_/N-t1o8k6nm?utm_source=instagram&utm_medium=social&utm_campaign=PUBL_MENS_PRECO_SS26_IG_WW_ENG_AW_20251024_20251102_MEN_RTW_MEN_PRECOLL_SS26',
+    buttonText: 'Shop the Pre-Collection'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_14.mp4',
+  duration: 28,
+  cta: {
+    message: `Coming October 16: <br>The Ultimate Body. Bigger butt, rounder hips, smaller waist, instant boob job—get ready to have it all with our most iconic solutions yet.`,
+    link: 'https://skims.com',
+    buttonText: 'Shop SKIMS'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_13.mp4',
+  duration: 88,
+  cta: {
+    message: `Just Dropped: <br>The Ultimate Bush. With our daring new Faux Hair Panty, your carpet can be whatever color you want it to be.`,
+    link: 'https://skims.com',
+    buttonText: 'Shop SKIMS'
+  },
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_21.mp4',
+  duration: 8,
+  cta: {
+    message: `No luck needed.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_1.mp4',
+  duration: 32,
+  cta: {
+    message: `Emotional Support Lip Balm <br><br> Meet the most hydrating lip balm you've ever had.<br>Finally, lip barrier care that hydrates & conditions with real skincare ingredients in a non-sticky, buttery smooth balm—all in an acne-safe formula so it won’t break you out.`,
+    link: 'https://www.personalday.com/collections/shop-all/products/emotional-support-lip-balm?fbclid=PAZXh0bgNhZW0CMTEAAadS-1QYIqX0_ITJw0M2d0o9gf6UJ3o78Ixs4Iem_2CGQ6znICCECrWuvAEdfw_aem_v-ufJ-YRIJ61cw6Ahlhzqw',
+    buttonText: 'Shop Personal Day '
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/sep/ad_47.mp4',
+  duration: 46,
+  cta: {
+    message: `Designed to sculpt. Engineered to perform.<br><br>A new brand for those who refuse to compromise.`,
+    link: 'https://skims.com/en-ng/collections/nikeskims',
+    buttonText: 'Shop'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_19.mp4',
+  duration: 15,
+  cta: {
+    message: `Don’t tell Saquon Barkley to stay grounded. He knows the sky is the limit.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_7.mp4',
+  duration: 30,
+  cta: {
+    message: `You can’t write this stuff. But LA just did again.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/oct/ad_25.mp4',
+  duration: 14,
+  cta: {
+    message: `There is no finish line when it comes to innovating to serve athletes.<br><br>Introducing four game - changing Nike innovations designed to transform what’ s possible.<br><br>Nike Mind<br>Nike Air Milano<br>Nike Aero - FIT<br>Nike Amplify<br>`,
+    link: 'https://empli.fi/nike/',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_8.mp4',
+  duration: 60,
+  cta: {
+    message: `Last-minute trip planning? <br>Before you hit panic button, hit that button on the remote instead.<br><br>
+With Samsung Vision AI Companion, you can explore destinations, plan activities, and discover local favorites - all from the comfort of your couch. ✈️🌴`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+},
+{
+  type: 'video',
+  url: 'https://github.com/freshBoyChilling/discography/raw/refs/heads/main/ads/2025/nov/ad_9.mp4',
+  duration: 54,
+  cta: {
+    message: `Like a boss, doing less means more at your home with Samsung AI Home.`,
+    link: '#',
+    buttonText: 'Learn More'
+  }
+}];
