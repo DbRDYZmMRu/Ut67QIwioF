5b1d4b4651982c83850aa4190eb38e8163a48df9
DbRDYZmMRu <frithmangrove@gmail.com>
2025-11-10 17:29:15 +0100

    Update generate_images.js

--- generate_images.js
+++ generate_images.js
@@ -7,12 +7,9 @@ registerFont('font/Architects_Daughter.ttf', { family: 'Architects Daughter' });
 
 const imageSize = 1080;
 const bgImagePath = 'https://raw.githubusercontent.com/DbRDYZmMRu/Ut67QIwioF/refs/heads/main/images/share/bg.jpg';
-const outputDir = path.join(__dirname, 'images', 'share', 'W2W', '18');
+const outputDir = path.join(__dirname, 'images', 'share', 'W2W', '19');
 const titles = [
- "I’m Awake Now",
-"X Chat",
-"Crevice",
-"Ghee Eyes, Butter Words"
+ "How Easy", "With No Helmet", "For a Stagehand", "Before the Show", "Where It Begins"
 
 
 ];
@@ -64,7 +61,7 @@ const generateImages = async () => {
     ctx.fillStyle = 'black';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'top';
-    ctx.fillText('WEST TO WEST COLLECTION XVIII', imageSize / 2, 10); // Corrected position
+    ctx.fillText('WEST TO WEST COLLECTION XIX', imageSize / 2, 10); // Corrected position
     
     // Save the image
     const buffer = canvas.toBuffer('image/png');
