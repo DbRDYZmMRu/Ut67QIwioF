b08e19625d17f0a10ffd3ebc55b76c0bb0256c4d
DbRDYZmMRu <frithmangrove@gmail.com>
2025-11-11 11:59:10 +0100

    Update generate_html.js

--- generate_html.js
+++ generate_html.js
@@ -3,12 +3,11 @@ const path = require('path');
 
 // Array of poetry titles
 const poetryTitles = [
-"In Training", "Culture", "Hoi Polloi", "Ptah’s Memphis"
-
+"Birthday Shower", "Mexicana", "Watermark"
 ];
 
 // Create necessary directories
-const shareDir = path.join(__dirname, 'share', 'W2W', 'XXIII');
+const shareDir = path.join(__dirname, 'share', 'W2W', 'XXI');
 fs.mkdirSync(shareDir, { recursive: true });
 
 // Template for the HTML content
@@ -89,16 +88,16 @@ const htmlTemplate = (title, description, keywords, ogImage, ogUrl, twitterImage
 
 // Generate HTML files
 poetryTitles.forEach((title, index) => {
-  const description = `${title} is a poem from West to West Collection XXIII by Howard Frith Hilton`;
-  const keywords = `${title}, West to West Collection XXIII`;
-  const ogImage = `https://frithhilton.com.ng/images/share/W2W/23/image_${index + 1}.png`;
-  const ogUrl = `https://www.frithhilton.com.ng/published/poetry/west-to-west-collection-XXIII.html?query=${index + 1}`;
+  const description = `${title} is a poem from West to West Collection XXI by Howard Frith Hilton`;
+  const keywords = `${title}, West to West Collection XXI`;
+  const ogImage = `https://frithhilton.com.ng/images/share/W2W/21/image_${index + 1}.png`;
+  const ogUrl = `https://www.frithhilton.com.ng/published/poetry/west-to-west-collection-XXI.html?query=${index + 1}`;
   const twitterImage = ogImage;
   const canonicalUrl = ogUrl;
   const redirectUrl = ogUrl;
   
   const htmlContent = htmlTemplate(
-    `${title} - West to West Collection XXIII`,
+    `${title} - West to West Collection XXI`,
     description,
     keywords,
     ogImage,
